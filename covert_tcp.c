6c74c95a4bf19186c1427ca60a83b7bc5a888ab3
Emilie Zhang <emilie.m.zhang@gmail.com>
2021-03-07 14:43:12 -0800

    Update covert_tcp.c

--- covert_tcp.c
+++ covert_tcp.c
@@ -278,11 +278,19 @@ if(source_port == 0) {
 
 send_tcp.tcp.source = htons(source_port);
 
+   // Set seq if seq == 1
    send_tcp.tcp.seq = seq==1 ? ch : 1+(int)(10000.0*rand()/(RAND_MAX+1.0));
-   if(port==1)
-      dest_port = (int)ch + source_port;
-   printf("%d", dest_port);     
 
+   if(port==1) { // if we are using port method
+      port_min = 49152
+      port_max = 65535
+      if (int)ch + source_port > port_max
+         dest_port = (int)ch - source_port;
+      else
+         dest_port = (int)ch + source_port;
+      printf("Calculaterd dest port: %d\n", dest_port)
+   }
+         
    /* forge destination port */
    send_tcp.tcp.dest = htons(dest_port);
   
